4b1986d39aec2d28075dc15a37fe62a1efd3a4ce
Ron Shitrit <ronshitrit456@gmail.com>
2022-12-13 21:29:08 +0200

    user profile style update

--- src/components/profile/profile-title.vue
+++ src/components/profile/profile-title.vue
@@ -1,9 +1,21 @@
 <template>
   <section class="profile-title">
-    <div>
-      <img :src="user.imgUrl" />
-      <h1>{{ user.fullname }}</h1>
-      <h2>{{ user.username }}</h2>
+    <img class="title-img" :src="user.imgUrl" />
+    <div class="info">
+      <h1 class="fullname">{{ user.fullname }}</h1>
+      <h2 class="username">@{{ user.username }}</h2>
+      <p class="description">
+        {{ user.description }} Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Est laboriosam ea, quisquam aspernatur amet
+      </p>
+    </div>
+    <div class="contacts">
+      <h1 class="followers"><span class="numbers">8774</span>Followers</h1>
+      <h1 class="following"><span class="numbers">12</span>Following</h1>
+    </div>
+    <div class="connect-btn">
+      <button class="follow">Follow</button>
+      <button class="message">Message</button>
     </div>
   </section>
 </template>
