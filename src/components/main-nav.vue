9a42923d0f358dcbf583705328ce55a9b06e7a42
Ron Shitrit <ronshitrit456@gmail.com>
2022-12-14 20:15:53 +0200

    nav and header style update

--- src/components/main-nav.vue
+++ src/components/main-nav.vue
@@ -1,19 +1,21 @@
 <template>
-  <nav class="main-nav">
-    <router-link to="/">Home</router-link>
-    <router-link v-if="!user" to="/auth/signup">Sign up</router-link>
-    <router-link v-if="!user" to="/auth/login">Log in</router-link>
-    <button v-else @click="toggleUserModal">
-      <img :src="user.imgUrl" />
-    </button>
-  </nav>
-
-  <div v-if="isUserModal && this.user" class="user-modal">
-    <h1>{{ user.fullname }}</h1>
-    <h2>{{ user.email }}</h2>
-    <router-link :to="`/profile/${user._id}`">Profile</router-link>
-    <button @click="logout">Log out</button>
-  </div>
+  <section class="main-nav">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/new-post">+</router-link>
+      <router-link v-if="!user" to="/auth/signup">Sign up</router-link>
+      <router-link v-if="!user" to="/auth/login">Log in</router-link>
+      <button v-else @click="toggleUserModal">
+        <img :src="user.imgUrl" />
+      </button>
+    </nav>
+    <div v-if="isUserModal && this.user" class="user-modal">
+      <h1>{{ user.fullname }}</h1>
+      <h2>{{ user.email }}</h2>
+      <router-link :to="`/profile/${user._id}`">Profile</router-link>
+      <button @click="logout">Log out</button>
+    </div>
+  </section>
 </template>
 
 <script>
